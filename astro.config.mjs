33a614a52c6676c42a75c32ec6eab64d70760d47
aido141176 <aiden@amcd.com.au>
2026-03-22 23:06:04 +0700

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
