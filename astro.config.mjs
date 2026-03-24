6b5a1ffa65fd62861c5cd761058492abfb0e7f8a
aido141176 <aiden@amcd.com.au>
2026-03-25 06:25:03 +0700

    update form config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-    site: 'https://yoursite.com',
+  site: 'https://astro-tina-cms.vercel.app',
+
+  output: 'server', // 🔥 IMPORTANT
+
   integrations: [sitemap()],
+
+  adapter: vercel(),
+
   vite: {
     plugins: [tailwindcss()],
   },
